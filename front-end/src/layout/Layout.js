6ea58e9b13367b53375ae96d9a8efbc8d789881d
Jeff Jones <jeffreydonovanjones@gmail.com>
2021-06-23 15:26:50 -0400

    new menu styles

--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -13,12 +13,11 @@ import "./Layout.css";
  */
 function Layout() {
   return (
-    <div className="container-fluid">
-      <div className="row h-100">
-        <div className="col-md-2 side-bar">
-          <Menu />
-        </div>
-        <div className="col">
+    <div className="container">
+      <Menu />
+
+      <div className="row">
+        <div>
           <Routes />
         </div>
       </div>
